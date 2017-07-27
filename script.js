7a43047691d7995fcac49d16bf1604047b7b44d4
njxmi <nicole.jones.mi@gmail.com>
2017-07-27 09:20:50 -0400

    additional changes made to indentaion

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //script for lab 8
 var items = [ 
-	{name: "beans", 				price: 0.99,		tax: false},
+	{name: "beans",					price: 0.99,		tax: false},
 	{name: "rice", 					price: 3.99,		tax: false},
 	{name: "flour", 				price: 1.99,		tax: false},
 	{name: "truffle oil", 			price: 7.25,		tax: false},
@@ -10,20 +10,19 @@ var items = [
 	{name: "almonds", 				price: 9.99,		tax: true}	
 ];
 	
-items.forEach(function(item){
-		
-	var food = document.createElement("li");
-		food.innerHTML = item.name + " $" + item.price.toFixed(2);
-		list.appendChild(food);
-		
+	items.forEach(function(item){	
+		var food = document.createElement("li");
+			food.innerHTML = item.name + " $" + item.price.toFixed(2);
+			list.appendChild(food);	
 });
 
-	var runningTotal = 0;
-		items.forEach(function(item) {	
+var runningTotal = 0;
+	items.forEach(function(item) {	
 		runningTotal += item.price; //price of each grocery
 		document.getElementById("total").innerHTML = "$" + runningTotal.toFixed(2);
 });
-	console.log ("$" + runningTotal.toFixed(2));
+
+console.log ("$" + runningTotal.toFixed(2));
 			 		 
 	
 // In Michigan sales tax is not actually collected on groceries, but may be used in this exercise regardless
